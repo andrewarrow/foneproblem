62286ba107bee4ee76dc306d644a423390931d58
Andrew Arrow <oneone@gmail.com>
2024-07-07 11:17:51 -0700

    fixed broken integration test

--- app/dashboard.go
+++ app/dashboard.go
@@ -10,6 +10,10 @@ func handleDashboard(c *router.Context) {
 	c.Title = "dashboard | foneproblem.com"
 	sql := fmt.Sprintf("where id in (select event_id from event_members where user_id=%d) order by created_at desc", c.User["id"])
 	items := c.All("event", sql, "")
+	for _, item := range items {
+		subitems := c.All("event_member", "where event_id=$1", "", item["id"])
+		item["members"] = subitems
+	}
 	send := map[string]any{"items": items}
 	c.SendContentInLayout("dashboard.html", send, 200)
 }
