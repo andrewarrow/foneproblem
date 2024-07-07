b43edc6990f39d8e0844b0664c703b433ac7de52
Andrew Arrow <oneone@gmail.com>
2024-07-07 11:08:09 -0700

    fixed security vulnerability

--- app/dashboard.go
+++ app/dashboard.go
@@ -4,6 +4,7 @@ import "github.com/andrewarrow/feedback/router"
 
 func handleDashboard(c *router.Context) {
 	c.Title = "dashboard | foneproblem.com"
-	send := map[string]any{}
+	items := c.All("event", "where id in (select user_id from event_members where user_id=$1) order by created_at desc", "")
+	send := map[string]any{"items": items}
 	c.SendContentInLayout("dashboard.html", send, 200)
 }
