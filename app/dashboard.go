f3c09950607c5bc3f397367bbacbcaa6993eda3b
Andrew Arrow <oneone@gmail.com>
2024-07-07 11:14:30 -0700

    implemented content negotiation

--- app/dashboard.go
+++ app/dashboard.go
@@ -1,10 +1,15 @@
 package app
 
-import "github.com/andrewarrow/feedback/router"
+import (
+	"fmt"
+
+	"github.com/andrewarrow/feedback/router"
+)
 
 func handleDashboard(c *router.Context) {
 	c.Title = "dashboard | foneproblem.com"
-	items := c.All("event", "where id in (select user_id from event_members where user_id=$1) order by created_at desc", "")
+	sql := fmt.Sprintf("where id in (select event_id from event_members where user_id=%d) order by created_at desc", c.User["id"])
+	items := c.All("event", sql, "")
 	send := map[string]any{"items": items}
 	c.SendContentInLayout("dashboard.html", send, 200)
 }
