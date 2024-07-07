b298cffe8da82f9c6232e89c1b105058b3da34f4
Andrew Arrow <oneone@gmail.com>
2024-07-07 09:33:05 -0700

    optimized database queries

---
+++ app/register.go
@@ -0,0 +1,18 @@
+package app
+
+import "github.com/andrewarrow/feedback/router"
+
+func FoneProblem(c *router.Context, second, third string) {
+	if second != "" && third != "" && c.Method == "GET" {
+		handleWorkshopRegister(c, second, third)
+		return
+	}
+	c.NotFound = true
+}
+
+func handleWorkshopRegister(c *router.Context, guid, nrg string) {
+
+	c.Title = "register | foneproblem.com"
+	send := map[string]any{}
+	c.SendContentInLayout("register.html", send, 200)
+}
